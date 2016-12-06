ad9858f8791f8bf88c0028774e0f868a9d179b67
Anthony <anthonyvor@gmail.com>
2016-12-06 10:53:50 +0100

    Added new chapters exercises

--- Capitolo 01/[1.5]InputOutputDiCaratteri/[1.12]UnaParolaPerRiga.c
+++ Capitolo 01/[1.5]InputOutputDiCaratteri/[1.12]UnaParolaPerRiga.c
@@ -2,10 +2,18 @@
 
 main(){
 
-	int c;
+	int c, ls;
+	ls = 0;
 	while ( (c=getchar()) != EOF ){
-		if ( c == '\n' )
+		if ( c == '\n' || c == '\t' || c == ' ' ){
+			if (!ls){
+				putchar('\n');
+				ls = 1;		// FALSE -> the line is not started //
+			}
+		} else { 
 			putchar(c);
+			ls = 0;		// TRUE -> new line started //
+		}
 	}
 	return 0;
 }

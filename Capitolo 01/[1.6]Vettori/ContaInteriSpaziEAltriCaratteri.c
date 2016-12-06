e003e5056d40006b8d3eb0b41e775652b5ddc223
Anthony <anthonyvor@gmail.com>
2016-12-06 11:01:47 +0100

    Aggiunta la cartella degli esercizi del paragrafo 1.6

---
+++ Capitolo 01/[1.6]Vettori/ContaInteriSpaziEAltriCaratteri.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+main()
+{
+	int c, i, nspazi, naltri; 	//Inizializzazione delle variabili per elaborare gli input, indicizzare l'array ninteri, contare gli spazi e contari gli altri caratteri
+	int ninteri[10];
+
+	nspazi = naltri = 0;
+	for ( i = 0; i < 10; ++i)
+		ninteri[i] = 0;
+	
+	while (( c = getchar() ) != EOF ){
+		if ( c >= '0' && c <= '9' )
+			++ninteri[c-'0'];
+		else if ( c == ' ' || c == '\n' || c == '\t' )
+			++nspazi;
+		else
+			++naltri;
+		
+		printf("Gli interi inseriti sono: ");
+		for ( i = 0; i < 10; ++i ){
+			if ( i == 9 ){
+				printf("\n");
+			printf(" %d", ninteri[i]);
+			}
+			}
+		printf("Il numero degli spazi è: %d\nIl numero degli altri caratteri è: %d\n, nspazi, naltri);
+	}
+}
